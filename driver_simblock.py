b336b78ca77d2308e24466bc4f62f9c9d978d7c8
amiecorso <acorso@cs.uoregon.edu>
2019-11-23 10:34:41 -0800

    updated plots.py

--- driver_simblock.py
+++ driver_simblock.py
@@ -3,6 +3,7 @@ import subprocess
 import shutil
 import argparse
 from datetime import datetime
+import numpy as np
 
 
 NET_PATH = "/Users/amiecorso/simblock/simulator/src/main/java/SimBlock/settings/NetworkConfiguration.java"
@@ -13,10 +14,22 @@ DATA_DIR = "/Users/amiecorso/scripts/data/"
 ARCHIVE_DIR = "/Users/amiecorso/scripts/archive/"
 
 # SETTING COMBINATIONS
-NUM_NODES = [1, 2, 4, 8, 16, 32, 64, 128] #[1, 2, 4, 8, 16, 32, 64, 128, 256]
-BLOCK_INTERVALS =[sec * 1000 for sec in [5, 10, 20, 30, 40, 50, 80, 100]] # milliseconds
+NUM_NODES = [5000] #[1, 2, 4, 8, 16, 32, 64, 128, 256]
+BLOCK_INTERVALS =[sec * 1000 for sec in np.arange(5, 300, 5)] # milliseconds
+BLOCK_SIZES = [535000] # bytes
+ENDBLOCKHEIGHT = 300
+'''
+NUM_NODES = [1, 4, 16, 32, 128] #[1, 2, 4, 8, 16, 32, 64, 128, 256]
+BLOCK_INTERVALS =[sec * 1000 for sec in np.arange(5, 600, 10)] # milliseconds
 BLOCK_SIZES = [535000] # bytes
 ENDBLOCKHEIGHT = 400
+'''
+''' tiny test
+NUM_NODES = [2] #[1, 2, 4, 8, 16, 32, 64, 128, 256]
+BLOCK_INTERVALS =[sec * 1000 for sec in [30]] # milliseconds
+BLOCK_SIZES = [535000] # bytes
+ENDBLOCKHEIGHT = 50 
+'''
 
 def write_sim_config(nodes, interval, blocksize, endblockheight):
     ''' Update the SimulationConfiguration.java file with given parameters'''
@@ -78,7 +91,7 @@ def calc_throughput(filepath):
     return 0
 
 
-def process_results(results_dir):
+def process_results(results_dir, outfile_name):
     ''' Generate CSV summary of data from output files '''
     results = []
     for f in os.listdir(results_dir):
@@ -90,7 +103,7 @@ def process_results(results_dir):
             results.append(','.join((str(nodes), str(interval), str(size), str(SBR), str(throughput))) + '\n')
     if not os.path.exists(DATA_DIR):
         os.mkdir(DATA_DIR)
-    with open(DATA_DIR + 'data.csv', 'w') as outfile:
+    with open(DATA_DIR + outfile_name, 'w') as outfile:
         outfile.write(','.join(('nodes', 'interval', 'blocksize', 'SBR', 'throughput')) + '\n')
         outfile.writelines(results)
 
@@ -106,7 +119,7 @@ def main():
 
     if not os.path.exists(RESULTS_DIR):
         os.mkdir(RESULTS_DIR)
-    if not os.path.exist(ARCHIVE_DIR):
+    if not os.path.exists(ARCHIVE_DIR):
         os.mkdir(ARCHIVE_DIR)
     for nodecount in NUM_NODES:
         for interval in BLOCK_INTERVALS:
@@ -114,8 +127,14 @@ def main():
                 write_sim_config(nodecount, interval, size, ENDBLOCKHEIGHT)
                 build_and_run()
                 blocklist_path, output_path = collect_outputs((nodecount, interval, size))
-    process_results(RESULTS_DIR)
+    now = ''.join('_'.join(str(datetime.now()).split()).split(".")[:-1])
+    datafilename = "data_" + now + ".csv"
+    process_results(RESULTS_DIR, datafilename)
     # move experimental results to archive dir
+    results_name = RESULTS_DIR.rstrip("/") + "_" + now
+    subprocess.run(["mv", RESULTS_DIR, results_name])
+    subprocess.run(["cp", "-r", results_name, ARCHIVE_DIR])
+    subprocess.run(["rm", "-rf", results_name])
 
 
 
---
+++ plots.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f')
+args = parser.parse_args()
+
+if args.f:
+    DATA = args.f
+else:
+    DATA = "./data/data.csv"
+df = pd.read_csv(DATA)
+df = df.sort_values(['nodes', 'interval'])
+print(df)
+
+netsizes = list(df["nodes"].unique())
+intervals = list(df["interval"].unique())
+#repeats = df["Run Index"].unique()
+repeats = [1]
+number_of_colors = max(len(netsizes), len(intervals))
+
+colors = ["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)]) for i in range(number_of_colors)]
+
+# plot Stale Block Rate
+fig, ax = plt.subplots()
+fig.suptitle("Stale Block Rate")
+for size in netsizes:
+    color = colors[netsizes.index(size)]
+    for run in repeats:
+        subset = df[df['nodes'] == size]
+        subset.plot(x="interval", y="SBR", ax=ax, label=str(size), color=color)
+ax.set(xlabel="Block Interval (ms)", ylabel="Stale Block Rate")
+fig = ax.get_figure()
+fig.savefig("/Users/amiecorso/Desktop/SBR.pdf")
+
+# plot throughput as a function of network size
+fig, ax = plt.subplots()
+fig.suptitle("Throughput v.s. Network Size")
+for interval in intervals:
+    color = colors[intervals.index(interval)]
+    for run in repeats:
+        subset = df[df['interval'] == interval]
+        subset.plot(x="nodes", y="throughput", ax=ax, label=str(interval), color=color)
+ax.set(xlabel="Network Size", ylabel="Throughput (bytes/sec)")
+fig = ax.get_figure()
+fig.savefig("/Users/amiecorso/Desktop/through_vs_netsize.pdf")
+
+# plot Throughput as a function of block interval
+fig, ax = plt.subplots()
+fig.suptitle("Throughput v.s. Block Interval")
+for size in netsizes:
+    color = colors[netsizes.index(size)]
+    for run in repeats:
+        subset = df[df['nodes'] == size]
+        subset.plot(x="interval", y="throughput", ax=ax, label=str(size), color=color)
+ax.set(xlabel="Block Interval (ms)", ylabel="Throughput (bytes/sec)")
+fig = ax.get_figure()
+fig.savefig("/Users/amiecorso/Desktop/through_vs_interval.pdf")
+
+
+plt.show()
